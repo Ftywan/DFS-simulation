f1ba36b865611c445c508d96b8994e3a90127f66
Tianyuan Fu <tianyuan_fu@apple.com>
2022-12-01 02:08:54 +0800

    Add experiment

--- peersim/src/ipfs/IPFSObserver.java
+++ peersim/src/ipfs/IPFSObserver.java
@@ -1,11 +1,27 @@
 package ipfs;
 
+import com.opencsv.CSVWriter;
 import ipfs.message.IPFSMessage;
 import ipfs.message.MessageStatus;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import peersim.config.Configuration;
 import peersim.core.Control;
+import peersim.util.IncrementalStats;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static ipfs.IPFSUtilities.globalRequestStatus;
 
@@ -23,6 +39,8 @@ public class IPFSObserver implements Control {
     private static final String PARAM_DROPRATE = "droprate";
     private static final String PARAM_MINDELAY = "mindelay";
     private static final String PARAM_MAXDELAY = "maxdelay";
+
+    private static String PATH;
     /**
      * Protocol identifier, obtained from config property
      */
@@ -31,6 +49,9 @@ public class IPFSObserver implements Control {
     private final double dropRate;
     private final int minDelay;
     private final int maxDelay;
+
+    File output;
+    FileWriter writer;
 
     /**
      * The basic constructor that reads the configuration file.
@@ -44,7 +65,14 @@ public class IPFSObserver implements Control {
         minDelay = Configuration.getInt(prefix + "." + PARAM_MINDELAY);
         maxDelay = Configuration.getInt(prefix + "." + PARAM_MAXDELAY);
 
-        System.out.println("A new instance");
+        PATH = "exp/" + netSize + "-" + dropRate + "-" + minDelay + "-" + maxDelay + "-60wire" + ".csv";
+
+        String[] header = {"time", "success", "flying", "dropped", "failed"};
+        try {
+            Files.writeString(Path.of(PATH), String.join(",", header) + "\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -55,24 +83,44 @@ public class IPFSObserver implements Control {
     @Override
     public boolean execute() {
 //        System.out.println("Observing");
-        int failCount = 0;
-        int successCount = 0;
-        int flying = 0;
-        int dropped = 0;
+//        int failCount = 0;
+//        int successCount = 0;
+//        int flying = 0;
+//        int dropped = 0;
 
-        for (Map.Entry<IPFSMessage, MessageStatus> entry : globalRequestStatus.entrySet()) {
-            if (entry.getValue() == MessageStatus.FLYING) {
-                flying++;
-            } else if (entry.getValue() == MessageStatus.SUCCESS) {
-                successCount++;
-            } else if (entry.getValue() == MessageStatus.DROPPED) {
-                dropped++;
-            } else if (entry.getValue() == MessageStatus.REJECTED) {
-                failCount++;
-            }
+//        for (Map.Entry<IPFSMessage, MessageStatus> entry : globalRequestStatus.entrySet()) {
+//            if (entry.getValue() == MessageStatus.FLYING) {
+//                flying++;
+//            } else if (entry.getValue() == MessageStatus.SUCCESS) {
+//                successCount++;
+//            } else if (entry.getValue() == MessageStatus.DROPPED) {
+//                dropped++;
+//            } else if (entry.getValue() == MessageStatus.REJECTED) {
+//                failCount++;
+//            }
+//        }
+        IncrementalStats stats = new IncrementalStats();
+        long time = peersim.core.CommonState.getTime();
+
+        Map<MessageStatus, Long> count = globalRequestStatus.values().stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+//        System.out.println(
+//                "success: " + count.getOrDefault(MessageStatus.SUCCESS, 0L) +
+//                " flying: " + count.getOrDefault(MessageStatus.FLYING, 0L) +
+//                " dropped: " + count.getOrDefault(MessageStatus.DROPPED, 0L) +
+//                " failed: " + count.getOrDefault(MessageStatus.REJECTED, 0L));
+        long[] row = {
+                time,
+                count.getOrDefault(MessageStatus.SUCCESS, 0L),
+                count.getOrDefault(MessageStatus.FLYING, 0L),
+                count.getOrDefault(MessageStatus.DROPPED, 0L),
+                count.getOrDefault(MessageStatus.REJECTED, 0L)
+        };
+        try {
+            Files.writeString(Path.of(PATH), Arrays.stream(row).mapToObj(String::valueOf).collect(Collectors.joining(","))+"\n", StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        System.out.println("success: " + successCount + " flying: " + flying + " dropped: " + dropped + " failed: " + failCount);
         return false;
     }
 }
