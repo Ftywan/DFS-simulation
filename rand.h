6c83da92cffdbbe79f612ad075c3d42a676c25f8
Fu Tianyuan <ftygary@gmail.com>
2022-12-15 08:34:14 +0800

    File clean

--- rand.h
+++
@@ -1,8 +0,0 @@
-/* the following three declarations are for use of the random number 
-   generator rand and the associated functions randst and randgt for 
-   seed management. This file (rand.h) should be included in any program
-   using these functions */
-float randno(int stream);
-void randst(long zset, int stream);
-long randgt(int stream);
-
